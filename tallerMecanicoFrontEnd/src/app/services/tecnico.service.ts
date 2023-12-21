52d032801a90bc35da4d6cd498400dbb5a79fedd
NicolasMamani <niko_mamani@hotmail.com>
2023-12-20 23:30:48 -0300

    solucionando merge

--- tallerMecanicoFrontEnd/src/app/services/tecnico.service.ts
+++ tallerMecanicoFrontEnd/src/app/services/tecnico.service.ts
@@ -5,66 +5,62 @@ import { environment } from 'src/enviroments/enviroment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TecnicoService {
-
-  private API_REST = environment.apiUrl
+  private API_REST = environment.apiUrl;
   private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
- /* @GetMapping
+  /* @GetMapping
     private ResponseEntity<List<Tecnico>> getAllTecnico(){
         return ResponseEntity.ok(this.tecnicoService.findAll());
     }
 */
-
-  public getAll():Observable<any>{
-    return this.http.get(`${this.API_REST}/tecnico/`);
-  }
 
   /* @PostMapping("filtrar/")
   private ResponseEntity<List<Tecnico>> search(@RequestBody Tecnico tecnicoRq){
       return ResponseEntity.ok(this.tecnicoService.filtrarTecnicos(tecnicoRq));
   } */
 
-  public onConsultar(tecnicoRq: Tecnico): Observable<any>{
-    return this.http.post(`${this.API_REST}/tecnico/filtrar/`, tecnicoRq, { headers: this.headers });
+  public onConsultar(tecnicoRq: Tecnico): Observable<any> {
+    return this.http.post(`${this.API_REST}/tecnico/filtrar/`, tecnicoRq, {
+      headers: this.headers,
+    });
   }
-  
+
   /* @PutMapping("update/")
   private ResponseEntity<Tecnico> update(@RequestBody Tecnico tecnicoRq){
       return ResponseEntity.ok(this.tecnicoService.update(tecnicoRq));
   } */
 
-  public onUpdate(tecnicoRq: Tecnico): Observable<any>{
-    return this.http.put(`${this.API_REST}/tecnico/update/`, tecnicoRq, { headers: this.headers });
+  public onUpdate(tecnicoRq: Tecnico): Observable<any> {
+    return this.http.put(`${this.API_REST}/tecnico/update/`, tecnicoRq, {
+      headers: this.headers,
+    });
   }
-  
+
   /* @DeleteMapping("delete/{id}")
   private ResponseEntity<Boolean> deleteById(@PathVariable("id") Long id){
       this.tecnicoService.deleteById(id);
       return ResponseEntity.ok(!(this.tecnicoService.getById(id) != null));
   } */
 
-  public onDeleteById(id: number): Observable<any>{
+  public onDeleteById(id: number): Observable<any> {
     return this.http.delete(`${this.API_REST}/tecnico/delete/${id}`);
   }
-  
+
   /* @PostMapping("save/")
   private ResponseEntity<Tecnico> saveTecnico(@RequestBody Tecnico tecnicoRq){
       return ResponseEntity.ok(this.tecnicoService.save(tecnicoRq));
   } */
-  public onGuardar(tecnicoRq: Tecnico): Observable<any>{
-    return this.http.post(`${this.API_REST}/tecnico/save/`, tecnicoRq, { headers: this.headers });
+  public onGuardar(tecnicoRq: Tecnico): Observable<any> {
+    return this.http.post(`${this.API_REST}/tecnico/save/`, tecnicoRq, {
+      headers: this.headers,
+    });
   }
 
-  public getAll():Observable<any>{
+  public getAll(): Observable<any> {
     return this.http.get(`${this.API_REST}/tecnico/`);
   }
-
- 
-
 }
