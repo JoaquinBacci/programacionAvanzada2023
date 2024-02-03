4238d385d24a2142051c86e80f1ffa342ae4d244
NicolasMamani <niko_mamani@hotmail.com>
2024-02-03 01:48:51 -0300

    agregando notificaciones en componentes reactivar

--- tallerMecanicoFrontEnd/src/app/dialogs/reactivar-vehiculo/reactivar-vehiculo.component.ts
+++ tallerMecanicoFrontEnd/src/app/dialogs/reactivar-vehiculo/reactivar-vehiculo.component.ts
@@ -2,6 +2,8 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Vehiculo } from 'src/app/model/vehiculo';
 import { VehiculoService } from 'src/app/services/vehiculo.service';
+import Toastify from 'toastify-js';
+
 
 @Component({
   selector: 'app-reactivar-vehiculo',
@@ -39,9 +41,39 @@ export class ReactivarVehiculoComponent implements OnInit{
       next: (data) => {
         console.log('Data: ', data);
         this.dataSource = data;
+        Toastify({
+          text: 'Vehiculo reactivado',
+          duration: 3000,
+          destination: 'https://github.com/apvarun/toastify-js',
+          newWindow: true,
+          close: true,
+          gravity: 'bottom', // Cambiado a "bottom" para colocarlo en la parte inferior
+          position: 'right', // Cambiado a "right" para colocarlo en la esquina derecha
+          stopOnFocus: true,
+          style: {
+            background: 'black', // Cambiado a negro
+          },
+          onClick: function () {},
+        }).showToast();
       },
       complete: () => {},
-      error: (error) => {},
+      error: (error) => {
+        Toastify({
+          text: 'Problema al reactivar el vehiculo',
+          duration: 3000,
+          destination: 'https://github.com/apvarun/toastify-js',
+          newWindow: true,
+          close: true,
+          gravity: 'bottom', // Cambiado a "bottom" para colocarlo en la parte inferior
+          position: 'right', // Cambiado a "right" para colocarlo en la esquina derecha
+          stopOnFocus: true,
+          style: {
+            background: 'red', // Cambiado a negro
+            color: 'black'
+          },
+          onClick: function () {},
+        }).showToast();
+      },
     });
   }
 
